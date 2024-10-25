0eb77832c7c6e543d595a2bba6584717595e8d3f
TONNY-tec <142325018+TONNY-tec@users.noreply.github.com>
2024-10-25 11:23:38 +0300

    Update Variablesinput.py

--- Variablesinput.py
+++ Variablesinput.py
@@ -13,29 +13,29 @@ c= 23.56
 is_girl = True
 is_boy = False
 # input from user
-# name = input("What is your name? ")
-# print (name, "  Is succesful recieved")
-# age = input("What is your age? ")
-# print(age)
-# height = input("What is your height? ")
-# print("Your height is",  height + "inches tall")
-# weight = input("What is your weight? ")
-# print("Your weight is ", weight)
-# # Type conversion
-# first_number = int(input("Please enter your first number: "))
-# second_number = int(input("Please enter your second number: "))
-# sum = first_number + second_number
-# print(sum)
-#
-# age1 = int(input("Please enter your age1: "))
-# age2 = int(input("Please enter your age2: "))
-# subtract = age1 - age2
-# print("Your age difference is ", subtract)
-#
-# y = float(input("First mark: "))
-# x = float(input("Second mark: "))
-# diff = y - x
-# print(diff)
+name = input("What is your name? ")
+print (name, "  Is succesful recieved")
+age = input("What is your age? ")
+print(age)
+height = input("What is your height? ")
+print("Your height is",  height + "inches tall")
+weight = input("What is your weight? ")
+print("Your weight is ", weight)
+# Type conversion
+first_number = int(input("Please enter your first number: "))
+second_number = int(input("Please enter your second number: "))
+sum = first_number + second_number
+print(sum)
+
+age1 = int(input("Please enter your age1: "))
+age2 = int(input("Please enter your age2: "))
+subtract = age1 - age2
+print("Your age difference is ", subtract)
+
+y = float(input("First mark: "))
+x = float(input("Second mark: "))
+diff = y - x
+print(diff)
 
 # take in a persons height and weight and calculate the persons BMI then display the BMI
 height1 = float(input("Enter the height"))
